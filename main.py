a822d1d26cc20c22f91dd88c2d2893e290a06017
username <mail@ex.com>
2026-06-11 17:47:44 +0300

    Убрал googletrans, добавил модель Whisper для приема ответа через голос

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import cv2
 import torch
-import asyncio
+import sounddevice as sd
+from scipy.io.wavfile import write
+from faster_whisper import WhisperModel
 from json import load
-from googletrans import Translator
 from PIL import Image
 from transformers import CLIPProcessor, CLIPModel, AutoTokenizer
 from model import VQACrossAttentionTransformer
@@ -20,20 +21,41 @@ model.to(device).eval()
 clip_model = CLIPModel.from_pretrained("openai/clip-vit-base-patch32").to(device)
 processor = CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
 BERT_model = AutoTokenizer.from_pretrained("bert-base-uncased")
+whisper_model = WhisperModel(
+    "small",
+    device=device,
+    compute_type="int8"
+)
 
 with open(path_to_ans2idx, 'r', encoding='utf-8') as f:
     ans2idx = load(f)
 idx2ans = {v: k for k, v in ans2idx.items()}
 
 
-async def translate_text(text, source, end):
-    async with Translator() as translator:
-        res = await translator.translate(text, src=source, dest=end)
-        return res.text
+def recognize_speech(duration=5):
+    fs = 16000
+    print("Говорите...")
+    audio = sd.rec(
+        int(duration * fs),
+        samplerate=fs,
+        channels=1,
+        dtype='int16',
+        device=2
+    )
+
+    sd.wait()
+    write("question.wav", fs, audio)
+    segments, _ = whisper_model.transcribe(
+        "question.wav",
+        task="translate"
+    )
+
+    text = " ".join(segment.text for segment in segments)
+    print("Распознано:", text)
+    return text
      
 
 def proceed_text_for_question(question):
-    question = asyncio.run(translate_text(question, 'ru', 'en'))
     with torch.no_grad():
         encoded = BERT_model(question, padding="max_length", truncation=True, max_length=max_question_length, return_tensors="pt")
     return encoded["input_ids"].to(device), encoded["attention_mask"].to(device)
@@ -52,7 +74,7 @@ def proceed_img_for_question(frame):
 cap = cv2.VideoCapture(0)
 current_answer = "Жду вопрос..."
 question_text = "Сколько людей в кадре?"
-print("Нажми 'q' для выхода, 'r' чтобы переспросить")
+print("Нажми 'q' для выхода, 'r' чтобы задать вопрос и получить ответ")
 
 while True:
     ret, frame = cap.read()
@@ -64,16 +86,15 @@ while True:
     key = cv2.waitKey(1)
 
     if key == ord('r'):
+        question_text = recognize_speech()
         print("Обработка...")
         patch_features = proceed_img_for_question(frame)
         q_tokens, attention_mask = proceed_text_for_question(question_text)
-        print(patch_features.shape)
-        print(q_tokens.shape)
-        print(attention_mask.shape)
         with torch.no_grad():
             logits = model(patch_features, q_tokens, attention_mask)
             current_answer = idx2ans[logits.argmax(dim=-1).item()]
-            print("Ответ:", asyncio.run(translate_text(current_answer, 'en', 'ru')))
+            print("Ответ:", current_answer)
+
     elif key == 27 or key == ord('q'): 
         break
 
